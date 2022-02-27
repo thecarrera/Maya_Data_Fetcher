290779449da7b9d98960d65cfca9e7f532d8c4e7
the_c <the_c@LAPTOP-BSNESOAM>
2022-02-27 17:15:05 +0100

    correcting pull mistakes

--- MayaProject/DirectX.h
+++ MayaProject/DirectX.h
@@ -9,63 +9,68 @@ class DX
 {
 private:
 	//Core
-	ComPtr<ID3D11Device>				gDevice						{};
-	ComPtr<ID3D11DeviceContext>			gDeviceContext				{};
-	ComPtr<IDXGISwapChain>				gSwapchain					{};
-	ComPtr<ID3D11RasterizerState>		gRasterizerState			{};
-	ComPtr<ID3D11Texture2D>				gDepthStencil[2]			{};
-	ComPtr<ID3D11DepthStencilView>		gDepthStencilView[2]		{};
-	ComPtr<ID3D11RenderTargetView>		gBackbufferRTV[2]			{};
+	ComPtr<ID3D11Device>				gDevice{};
+	ComPtr<ID3D11DeviceContext>			gDeviceContext{};
+	ComPtr<IDXGISwapChain>				gSwapchain{};
+	ComPtr<ID3D11RasterizerState>		gRasterizerState{};
+	ComPtr<ID3D11Texture2D>				gDepthStencil[2]{};
+	ComPtr<ID3D11DepthStencilView>		gDepthStencilView[2]{};
+	ComPtr<ID3D11DepthStencilState>		gDepthStencilState{};
+	ComPtr<ID3D11RenderTargetView>		gBackbufferRTV[2]{};
 
 	//Default Textures
-	ComPtr<ID3D11Texture2D>				defaultTextureMap			{};
-	ComPtr<ID3D11ShaderResourceView>	defaultTextureSRV			{};
-	ComPtr<ID3D11ShaderResourceView>	nullSRV						{};
+	ComPtr<ID3D11Texture2D>				defaultTextureMap{};
+	ComPtr<ID3D11ShaderResourceView>	defaultTextureSRV{};
+	ComPtr<ID3D11ShaderResourceView>	nullSRV{};
 
 	//G-Buffer
-	ComPtr<ID3D11Texture2D>				gGBufferTextures[3]			{};
-	ComPtr<ID3D11RenderTargetView>		gGBufferRTVs[3]				{};
-	ComPtr<ID3D11ShaderResourceView>	gGBufferSRVs[3]			{};
+	ComPtr<ID3D11Texture2D>				gGBufferTextures[3]{};
+	ComPtr<ID3D11RenderTargetView>		gGBufferRTVs[3]{};
+	ComPtr<ID3D11ShaderResourceView>	gGBufferSRVs[3]{};
 
+	//Light pass
+	ComPtr<ID3D11Buffer>				gLightPassQuadBuffer{};
+	ComPtr<ID3D11Buffer>				gLightPassQuadIDBuffer{};
 
-	struct ACTIVEPOINTLIGHTS{
-		UINT32 pointLightCount {};
-		NODETYPES::PointLight::POINTDATA pointLight[20] {};
+	struct ACTIVEPOINTLIGHTS {
+		UINT32 pointLightCount{};
+		NODETYPES::PointLight::POINTDATA pointLight[20]{};
 	}activePointLights{};
 
 	//Shaders
-	ComPtr<ID3D11InputLayout>			gVertexLayout				{};
-	ComPtr<ID3D11VertexShader>			gGBufferVertexShader		{};
-	ComPtr<ID3D11GeometryShader>		gGBufferGeometryShader		{};
-	ComPtr<ID3D11PixelShader>			gGBufferPixelShader			{};
+	ComPtr<ID3D11InputLayout>			gVertexLayout{};
+	ComPtr<ID3D11VertexShader>			gGBufferVertexShader{};
+	ComPtr<ID3D11GeometryShader>		gGBufferGeometryShader{};
+	ComPtr<ID3D11PixelShader>			gGBufferPixelShader{};
 
-	ComPtr<ID3D11PixelShader>			gLightPassShader			{};
+	ComPtr<ID3D11PixelShader>			gLightPassShader{};
 
-	ComPtr<ID3D11SamplerState>			txSamplerState				{};
-	
-	ComLib*								comlib						{};
+	ComPtr<ID3D11SamplerState>			txSamplerState{};
 
-	bool currentFrame {}; // currentFrame = !currentFrame
+	ComLib* comlib{};
+
+	bool currentFrame{}; // currentFrame = !currentFrame
 
 	// UP-CAST <-> DOWN-CAST
-	std::vector<std::pair<NODETYPES::Node*, std::string>>		pureNodes				{};
+	std::vector<std::pair<NODETYPES::Node*, std::string>>		pureNodes{};
 
-	std::vector<std::shared_ptr<NODETYPES::Camera>>				cameras					{};
-	NODETYPES::Camera*											activeCamera			{};
-	ComPtr<ID3D11Buffer>										cameraBuffer[2]			{}; // 128-bit - MAT(16-bit Aligned)
+	std::vector<std::shared_ptr<NODETYPES::Camera>>				cameras{};
+	NODETYPES::Camera* activeCamera{};
+	ComPtr<ID3D11Buffer>										cameraBuffer[2]{}; // 128-bit - MAT(16-bit Aligned)
 
-	std::vector<std::shared_ptr<NODETYPES::Mesh>>				meshes					{};
-	std::vector<std::shared_ptr<NODETYPES::Transform>>			transforms				{};
-	ComPtr<ID3D11Buffer>										transformBuffer			{}; // 64-bit - XMMATRIX(16-bit Aligned)
+	std::vector<std::shared_ptr<NODETYPES::Mesh>>				meshes{};
+	std::vector<std::shared_ptr<NODETYPES::Transform>>			transforms{};
+	ComPtr<ID3D11Buffer>										transformBuffer{}; // 64-bit - XMMATRIX(16-bit Aligned)
 
-	std::vector<std::shared_ptr<NODETYPES::PointLight>>			pointLights				{};
-	ComPtr<ID3D11Buffer>										pointLightDataBuffer	{};  
+	std::vector<std::shared_ptr<NODETYPES::Mesh>>				lightQuads{};
+	std::vector<std::shared_ptr<NODETYPES::PointLight>>			pointLights{};
+	ComPtr<ID3D11Buffer>										pointLightDataBuffer{};
 
-	std::vector<std::shared_ptr<NODETYPES::Texture>>			textures				{};
-	std::vector<std::shared_ptr<NODETYPES::Bump>>				bumps					{};
-	std::vector<std::shared_ptr<NODETYPES::Lambert>>			lamberts				{};
-	std::vector<std::shared_ptr<NODETYPES::Blinn>>				blinns					{};
-	std::vector<std::shared_ptr<NODETYPES::ShadingEngine>>		shadingEngines			{};
+	std::vector<std::shared_ptr<NODETYPES::Texture>>			textures{};
+	std::vector<std::shared_ptr<NODETYPES::Bump>>				bumps{};
+	std::vector<std::shared_ptr<NODETYPES::Lambert>>			lamberts{};
+	std::vector<std::shared_ptr<NODETYPES::Blinn>>				blinns{};
+	std::vector<std::shared_ptr<NODETYPES::ShadingEngine>>		shadingEngines{};
 
 
 public:
@@ -82,7 +87,7 @@ public:
 	void OfflineCreation(HMODULE hModule, HWND* wndHandle);
 	void Update();
 	void Render();
-	
+
 	void ClearGBufferTargets();
 	void RenderGBuffer();
 	void UpdateTransformBuffer(size_t index);
@@ -133,12 +138,15 @@ private:
 
 	void createDefaultTextures();
 	void CreateSamplerStates();
+	void CreateLightPassQuad();
 	void CreateBuffers();
 	HRESULT CreateDeviceSwapchain(HWND* wndHandle);
 	HRESULT CreateDepthBuffer(int index);
 	HRESULT CreateDepthStencil(int index, ID3D11Texture2D* pBackBuffer);
+	HRESULT CreateDepthStencilState();
 	HRESULT CreateGBufferResources();
 	HRESULT CreateRasterizerState();
+	void setScissorRect();
 	void setViewPort();
 
 	void CreateShaders();
